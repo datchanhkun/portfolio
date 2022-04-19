bf845ea9cd0e4d2f2b0866af13785960ce465c56
datchanhkun <caothanhdat.org@gmail.com>
2022-04-20 00:26:15 +0700

    portfolio & animation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Intro from './components/Intro/Intro';
 import MyStory from './components/MyStory/MyStory';
 import Navbar from './components/Navbar/Navbar';
-
+import Portfolio from './components/Portfolio/Portfolio'
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
       <MyStory />
+      <Portfolio />
     </div>
   );
 }
