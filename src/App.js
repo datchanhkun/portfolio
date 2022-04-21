29c962f77c218a58a540ce63f4d3da60b1631978
datchanhkun <caothanhdat.org@gmail.com>
2022-04-22 01:51:30 +0700

    contact

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import Navbar from './components/Navbar/Navbar';
 import Portfolio from './components/Portfolio/Portfolio'
 import MySkills from './components/MySkills/MySkills'
 import Projects from './components/Projects/Projects'
+import Contact from './components/Contact/Contact'
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Intro />
-      <MyStory />
-      <Portfolio />
-      <MySkills />
-      <Projects />
-    </div>
+    <>
+      <div className="App">
+        <Navbar />
+        <Intro />
+        <MyStory />
+        <Portfolio />
+        <MySkills />
+        <Projects />
+      </div>
+      <Contact />
+    </>
+
   );
 }
 
