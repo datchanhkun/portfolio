2570ddf7cb0630bfef84417b75fd06e1fffa285b
datchanhkun <caothanhdat.org@gmail.com>
2022-04-22 03:41:51 +0700

    footer & add animation for all

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer-wrapper'>
+      <div className='footer'>
+        <p>Built by Cao Thanh Dat and refer to Alissa's desgin.</p>
+        <p>Copyright 	&copy; 2022 All Rights Reserved.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
